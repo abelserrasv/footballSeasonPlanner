b5f42116a1d311421b8f2eeaf604a5be472a5ec6
Abel Serra <abel.serra@jambit.com>
2016-09-16 18:37:11 +0200

    TableView added and TableView dataSource implemented.

--- aufgabeSpielplan/ViewController.m
+++ aufgabeSpielplan/ViewController.m
@@ -6,24 +6,59 @@
 //  Copyright © 2016 Abel Serra. All rights reserved.
 //
 
+#import "SPSeason.h"
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property SPSeason *season;
+@property (weak, nonatomic) IBOutlet UITableView *matchesTableView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.season = [[SPSeason alloc] init];
+
+    self.title = [NSString stringWithFormat:@"%@ (%@)", self.season.leagueName, self.season.seasonName];
+
+    self.matchesTableView.dataSource = self;
+    self.matchesTableView.delegate = self;
 }
 
+#pragma mark - TableView Data Source
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.season getNumberOfMatchDays];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.season getNumberOfMatchesForADay:section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+
+    cell.textLabel.text = [self.season getMatchStringForADay:indexPath.section andMatch:indexPath.row];
+    return cell;
+}
+
+- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [self.season getStringForMatchDate:section];
+}
 
 @end
